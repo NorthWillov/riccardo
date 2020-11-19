90d233dbba6ef8a49548f46cbf7429dcc73625db
Artem <artem@artemsMac.local>
2020-11-19 23:17:02 +0100

    Add pizzas data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { MENU } from "../utils/constants";
 import "../styles/App.css";
 
 function App() {
+  console.log(MENU);
   return (
     <div className="App">
       <div className="container">
         <Navbar />
+        {MENU.pizzas.map((pizza) => (
+          <div>
+            <img srcSet={pizza.image}></img>
+            <h1>{pizza.name}</h1>
+            <p>Od {pizza.price.small} pln</p>
+            <div>
+              {pizza.ingredients.map((i) => (
+                <span>{i + " "}</span>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/constants.js
@@ -0,0 +1,368 @@
+export const MENU = {
+  pizzas: [
+    {
+      id: 1,
+      name: "Margherita",
+      price: {
+        small: "9.50",
+        medium: "16.90",
+        big: "32.50",
+      },
+      ingredients: ["sos", "ser", "oregano"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 2,
+      name: "Massima",
+      price: {
+        small: "14.50",
+        medium: "17.90",
+        big: "34.50",
+      },
+      ingredients: ["sos", "ser", "pieczarki", "cebula"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 3,
+      name: "Capricciosa",
+      price: {
+        small: "14.50",
+        medium: "20.90",
+        big: "40.50",
+      },
+      ingredients: ["sos", "ser", "szynka", "pieczarki"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 4,
+      name: "Bambino",
+      price: {
+        small: "15.50",
+        medium: "20.90",
+        big: "40.50",
+      },
+      ingredients: ["sos", "ser", "szynka", "ananas"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 5,
+      name: "Salami",
+      price: {
+        small: "15.50",
+        medium: "20.90",
+        big: "41.90",
+      },
+      ingredients: ["sos", "ser", "salami", "papryka", "ogórek"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 6,
+      name: "Salami",
+      price: {
+        small: "16.50",
+        medium: "20.90",
+        big: "42.50",
+      },
+      ingredients: ["sos", "ser", "tuńczyk", "czerwona cebula", "feta"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 7,
+      name: "Gamberetto",
+      price: {
+        small: "16.50",
+        medium: "22.90",
+        big: "43.50",
+      },
+      ingredients: ["sos", "ser", "szynka", "krewetki", "ananas"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 8,
+      name: "Wegetariana",
+      price: {
+        small: "16.50",
+        medium: "21.90",
+        big: "43.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "pieczarki",
+        "papryka",
+        "oliwki",
+        "ruccola",
+        "kukurydza",
+        "pomidory",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 9,
+      name: "Wiosenna",
+      price: {
+        small: "16.50",
+        medium: "21.90",
+        big: "43.50",
+      },
+      ingredients: ["sos", "ser", "brokuły", "cukinia", "krewetki", "czosnek"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 10,
+      name: "Broccoli",
+      price: {
+        small: "16.50",
+        medium: "21.90",
+        big: "43.50",
+      },
+      ingredients: ["sos", "ser", "brokuły", "pomidor", "feta", "parmezan"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 11,
+      name: "Pepperone",
+      price: {
+        small: "16.50",
+        medium: "22.50",
+        big: "44.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "bekon",
+        "papryka jalapeño",
+        "pieczarki",
+        "cebula",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 12,
+      name: "Forte",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "44.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "salami",
+        "kiełbasa pepperoni",
+        "papryka jalapeño",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 13,
+      name: "Wiejska",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "44.50",
+      },
+      ingredients: ["sos", "ser", "kiełbasa", "cebula", "pieczarki", "tabasco"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 14,
+      name: "Saporito",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "46.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "szynka",
+        "kurczak",
+        "pieczarki",
+        "ogórek",
+        "oregano",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 15,
+      name: "Szpinakowa",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "46.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "szpinak",
+        "kurczak",
+        "pomidory suszone",
+        "parmezan",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 16,
+      name: "Frutti di Mare",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "49.50",
+      },
+      ingredients: ["sos", "ser", "owoce morze", "kapary", "czosnek", "oliwki"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 17,
+      name: "Jalapeño",
+      price: {
+        small: "17.50",
+        medium: "23.50",
+        big: "49.50",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "szynka",
+        "bekon",
+        "papryka jalapeño",
+        "czosnek",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 18,
+      name: "Calzone (Pierog)",
+      price: {
+        small: "23.50",
+      },
+      ingredients: ["sos", "ser", "kurczak", "szynka", "pieczarki"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 19,
+      name: "Miscela",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "45.50",
+      },
+      ingredients: ["sos", "ser", "pieczarki", "szynka", "bekon", "oliwki"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 20,
+      name: "Salmone",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "46.90",
+      },
+      ingredients: ["sos", "ser", "łosoś wędzony", "ruccola", "parmezan"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 21,
+      name: "Ricco",
+      price: {
+        small: "16.50",
+        medium: "23.50",
+        big: "46.90",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "bekon",
+        "kiełbasa",
+        "pieczarki",
+        "kukurydza",
+        "cebula",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 22,
+      name: "Fantazja",
+      price: {
+        small: "18.50",
+        medium: "25.50",
+        big: "50.90",
+      },
+      ingredients: ["sos", "ser", "5 własnych składników do wyboru"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 23,
+      name: "Fantazja",
+      price: {
+        small: "17.50",
+        medium: "25.50",
+        big: "50.90",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "kurczak",
+        "kurki",
+        "cebula",
+        "ser pleśniowy",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 24,
+      name: "Wołowina",
+      price: {
+        small: "17.50",
+        medium: "25.50",
+        big: "50.90",
+      },
+      ingredients: [
+        "sos",
+        "ser",
+        "wołowina",
+        "feta",
+        "czerwona cebula",
+        "ogórek kiszony",
+      ],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+    {
+      id: 25,
+      name: "Bekonowe Jaja",
+      price: {
+        small: "16.50",
+        medium: "21.90",
+        big: "43.90",
+      },
+      ingredients: ["sos", "ser", "bekon", "szczypior", "jaja"],
+      image:
+        "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    },
+  ],
+};
