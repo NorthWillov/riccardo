bd41367a2995fcb9a43f15f582b0c2029096ff74
Artem <artem@artemsMac.local>
2020-11-19 19:34:41 +0100

    Add Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
+import react from "react";
+import Navbar from "./Navbar";
 import "../styles/App.css";
-import logo from "../images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbarr">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3">
-              <img className="logo" srcSet={logo} alt="riccardo-logo" />
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
     </div>
   );
 }
