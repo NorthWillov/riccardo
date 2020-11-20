8b7516b2100486984020fa6aabf794e2a1c32ed7
Artem <artem@artemsMac.local>
2020-11-20 16:13:34 +0100

    Add card

--- src/components/PizzasList.js
+++ src/components/PizzasList.js
@@ -7,15 +7,28 @@ export default function PizzasList() {
       <h3 className="title">Pizzy</h3>
       <div className="row">
         {MENU.pizzas.map((pizza) => (
-          <div className="col-md-4" key={pizza.id}>
-            <div className="pizza-card">
-              <img srcSet={pizza.image} alt={pizza.name}></img>
-              <h1>{pizza.name}</h1>
-              <p>Od {pizza.price.small} pln</p>
-              <div>
-                {pizza.ingredients.map((i, idx) => (
-                  <span key={i}>{pizza.ingredients[idx + 1] ? i + ", " : i}</span>
-                ))}
+          <div className="col-md-6 col-lg-4" key={pizza.name}>
+            <div className="card">
+              <img
+                className="card-img-top"
+                srcSet={pizza.image}
+                alt={pizza.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{pizza.name}</h5>
+                <p className="card-text">
+                  {pizza.ingredients.map((i, idx) => (
+                    <span key={i}>
+                      {pizza.ingredients[idx + 1] ? i + ", " : i}
+                    </span>
+                  ))}
+                </p>
+                <div className="checkout">
+                  <p className="checkout-price">od {pizza.price.small}pln</p>
+                  <a href="#" className="btn btn-primary">
+                    Wybierz
+                  </a>
+                </div>
               </div>
             </div>
           </div>
