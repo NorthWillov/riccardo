a2621bc9ea8dd314447094f765211fc5566035d1
Artem <northwillov@gmail.com>
2020-12-01 15:16:54 +0100

    Resize images

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,36 +4,38 @@ import { Carousel } from "react-bootstrap";
 import officeImg from "../images/office.jpg";
 import studentsImg from "../images/students.jpg";
 import familyImg from "../images/family.jpg";
+import "../styles/landing.css";
 
 function Landing(props) {
   return (
     <>
-      <Link to="/riccardo/pizzas">Pizzas</Link>
-      <Carousel>
+      <Carousel className="Carousel">
         <Carousel.Item interval={5000}>
           <img className="d-block w-100" src={officeImg} alt="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Rabaty dla firm</h3>
+            <p>
+              -5% na wszystkie dania. Przy większych zamówieniach negocjujemy
+              ceny.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img className="d-block w-100" src={studentsImg} alt="Second slide" />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Studenci jedzą taniej</h3>
+            <p>-5% na wszystkie dania</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img className="d-block w-100" src={familyImg} alt="Third slide" />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>Rodzinne obiadki</h3>
+            <p>-5% przy zamówieniu powyżej 70 zł (w lokalu)</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      <h3 className="mt-5">Popularne:</h3>
     </>
   );
 }
