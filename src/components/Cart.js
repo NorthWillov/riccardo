12de9c18ddc8c3387968ac5986eeae6845d7eabd
NorthWillov <northwillov@gmail.com>
2021-01-04 17:30:05 +0100

    Add basic cart products display for pizzas

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,21 @@
 import React, { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
 import { MENU } from "../utils/constants";
+import { Table } from "react-bootstrap";
+import { withStyles } from "@material-ui/styles";
+import styles from "../styles/cartStyles";
 
 function Cart(props) {
+  const { classes } = props;
+
   const { cart } = useContext(CartContext);
   console.log(cart);
-
-  const findDeletedIngredients = (pizza, pizzaId) => {
-    pizza.ingredients.map((i) => {
-      return MENU.pizzas[pizzaId + 1].ingredients.includes(i) ? "" : i;
-    });
-  };
 
   return (
     <div>
       <h1>CART:</h1>
 
-      {cart.map(
+      {/* {cart.map(
         (item, idx) =>
           item.type === "pizza" && (
             <div key={idx}>
@@ -34,9 +33,65 @@ function Cart(props) {
               <h4>{item.price} PLN</h4>
             </div>
           )
-      )}
+      )} */}
+      <Table responsive>
+        <tbody>
+          {cart.map(
+            (item, idx) =>
+              item.type === "pizza" && (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>
+                    <img
+                      style={{ width: "50px" }}
+                      src={item.image}
+                      alt="product"
+                    />
+                    <h6>{item.name}</h6>
+                    <p>{item.size}</p>
+                    <p>{item.dough}</p>
+                    <p>
+                      Bez:
+                      {MENU.pizzas[item.id - 1].ingredients.map((i) => {
+                        return item.ingredients.includes(i) ? "" : i;
+                      })}
+                    </p>
+                    <p>Dodaj: {item.extras.map((e) => e.name)}</p>
+                  </td>
+                  <td>
+                    <div className={classes.productCount}>
+                      <button className={classes.buttonCount}>-</button>
+                      <input
+                        type="text"
+                        readOnly
+                        className={classes.numberProduct}
+                        value="1"
+                      />
+                      <button className={classes.buttonCount}>+</button>
+                    </div>
+                  </td>
+                  <td>
+                    <h5>{item.price} PLN</h5>
+                  </td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash2-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225zm9.89-.69C10.966 2.214 9.578 2 8 2c-1.58 0-2.968.215-3.926.534-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466-.18-.14-.498-.307-.975-.466z" />
+                    </svg>
+                  </td>
+                </tr>
+              )
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 }
 
-export default Cart;
+export default withStyles(styles)(Cart);
---
+++ src/styles/cartStyles.js
@@ -0,0 +1,35 @@
+const styles = {
+  productCount: {
+    height: "30px",
+    "& a": {
+      textDecoration: "none",
+      fontWeight: "700",
+      color: "black",
+    },
+  },
+  buttonCount: {
+    display: "inlineBlock",
+    width: "30px",
+    height: "30px",
+    backgroundColor: "#2196F3",
+    color: "white",
+    fontSize: "24px",
+    lineHeight: "30px",
+    textAlign: "center",
+    border: "none",
+    outline: "none",
+    "&:active": {
+      backgroundColor: "#1565C0",
+    },
+  },
+  numberProduct: {
+    display: "inlineBlock",
+    width: "46px",
+    height: "28px",
+    fontSize: "24px",
+    border: "1px solid silver",
+    textAlign: "center",
+  },
+};
+
+export default styles;
